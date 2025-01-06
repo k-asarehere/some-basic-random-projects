58ed3b80d0eb3f8512e13e414854e2c26e069e66
Kwasi <ampsonasare@gmail.com>
2025-01-06 14:01:30 -0600

    Add files via upload

---
+++ time_countdown.py
@@ -0,0 +1,13 @@
+import time 
+
+def countdown(t):
+    while t : 
+        mins , secs = divmod(t,60)
+        timer_format = f'{mins:02d}:{secs:02d}'
+        print(timer_format, end='\r')
+        time.sleep(1)
+        t -= 1 
+    print('Countdown done!')
+
+countdown_num = int(input('Enter a number to countdown: '))
+countdown(countdown_num)
